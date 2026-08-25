cbc5f6e9fb3c7fc4d78d3eef763d8561b28d3638
nurnahian <nurnahian007@gmail.com>
2026-08-25 11:09:38 +0600

    feat:husky setup with commit rules

---
+++ backend/commitlint.config.js
@@ -0,0 +1,29 @@
+export default {
+  extends: ["@commitlint/config-conventional"],
+
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "chore",
+        "docs",
+        "style",
+        "refactor",
+        "perf",
+        "test",
+        "ci",
+        "build",
+        "revert",
+      ],
+    ],
+
+    "subject-case": [2, "always", "lower-case"],
+    "subject-max-length": [2, "always", 100],
+    "subject-full-stop": [2, "never", "."],
+    "header-max-length": [2, "always", 130],
+    "body-max-line-length": [0],
+  },
+};
